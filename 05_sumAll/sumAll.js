e4b9b0de1ac50256fb01db43c966632d2bfc692d
edrassimoes <edrassimoes@gmail.com>
2024-11-08 09:54:28 -0300

    Finish fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,32 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let numbers = [];
+    let result = 0;
+
+    if (Number.isNaN(start) || Number.isNaN(end)) {
+        return "ERROR"
+    }
+
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR"
+    }
+
+    if (start < 0 || end < 0) {
+        return 'ERROR'
+    }
+
+    if (start > end) {
+        [start, end] = [end, start];
+    }
+
+    for (let i = start; i < end + 1; i++) {
+        numbers.push(i)
+    }
+
+    for (let i = 0; i < numbers.length; i++) {
+        result = result + numbers[i];
+    }
+
+    return result;
 
 };
 
