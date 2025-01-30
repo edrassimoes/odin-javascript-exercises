cf244c4e263a155b8a5270089fd2146903817c1b
Edras Simões <edrassimoes@MacBook-Pro-de-Edras.local>
2025-01-30 17:06:56 -0300

    finish 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((acc, number) => acc + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, number) => acc * number, 1);
 };
 
-const power = function() {
-	
+const power = function(firstNumber, secondNumber) {
+  return Math.pow(firstNumber, secondNumber);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 1 || number === 0) {
+      return 1;
+    }
+    return number * factorial(number - 1);
 };
 
 // Do not edit below this line
