51ae8a6114f0afd992ee16b1c91292f2c4b5396b
Edras Simões <edrassimoes@MacBook-Pro-de-Edras.local>
2025-01-30 17:43:24 -0300

    finish 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const arr = word.toLocaleLowerCase().split('');
+    const treatedArr = arr.map(word => word.match(/[a-z0-9]/i));
+    const cleanedWord = treatedArr.join('');
+    const reversedWord = treatedArr.reverse().join('');
+    return cleanedWord === reversedWord;
 };
 
 // Do not edit below this line
