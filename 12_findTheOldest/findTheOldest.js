27a317f0574a40c752a50bdc11a247f567859583
edrassimoes <edrassimoes@gmail.com>
2025-01-31 15:45:55 -0300

    finish 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let oldest = 0;
+    let obj = {}
+    arr.forEach(person => {
+        let age = person.yearOfDeath - person.yearOfBirth
+        if (age > oldest) {
+            oldest = age;
+            obj = person;
+        }
+    })
+    return obj;
 };
 
 // Do not edit below this line
