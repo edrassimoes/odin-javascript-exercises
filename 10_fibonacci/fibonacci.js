2cc18be6f5dec359958df1d1cbbc7cc6c0dbde26
Edras Simões <edrassimoes@MacBook-Pro-de-Edras.local>
2025-01-31 15:07:35 -0300

    finish 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+    let fibonacci = [];
+    const number = Number(value);
 
+    if (number === 0) {
+        return 0;
+    }
+
+    if (number < 0) {
+        return 'OOPS';
+    }
+
+    for (let i = 0; i < number; i++) {
+        if (i < 2) {
+            fibonacci.push(1);
+        } else {
+            fibonacci.push(
+                fibonacci[i-2] + fibonacci[i-1]
+            );
+        }
+    }
+    return fibonacci[number - 1];
 };
 
 // Do not edit below this line
